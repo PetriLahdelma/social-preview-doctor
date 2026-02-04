e51008ce96349a3d687dc5ed864ce070aba2cbe1
Petri Lahdelma <petri.lahdelma@gmail.com>
2026-02-05 01:22:20 +0200

    chore: add src/lib/headers.ts

---
+++ src/lib/headers.ts
@@ -0,0 +1,21 @@
+import { BotName } from "./types.js";
+
+const COMMON = {
+  accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "en-US,en;q=0.9",
+  range: "bytes=0-1048576"
+};
+
+const AGENTS: Record<BotName, string> = {
+  linkedin: "LinkedInBot/1.0 (+http://www.linkedin.com)",
+  twitter: "Twitterbot/1.0",
+  facebook: "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)"
+};
+
+export function getHeaders(bot: BotName): Record<string, string> {
+  return {
+    ...COMMON,
+    "user-agent": AGENTS[bot]
+  };
+}
